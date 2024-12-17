c854a1516d4e61e9ea87eeb7f5f170403a60b51b
SergioMosesR <sergiomoses.r@gmail.com>
2024-12-17 08:33:08 +0700

    Test

--- database/seeders/UserTest.php
+++ database/seeders/UserTest.php
@@ -17,39 +17,15 @@ class UserTest extends Seeder
         $UserData = [
 
             [
-                'name' => 'Fitri',
-                'email' => 'fitri@gmail.com',
-                'password' => 'kasir123',
-                'role' => 'cashier'
-            ],
-            [
-                'name' => 'Dev Cashier',
-                'email' => 'thanos@gmail.com',
-                'password' => 'dev123',
-                'role' => 'cashier'
-            ],
-            [
-                'name' => 'Ara',
-                'email' => 'ara@gmail.com',
-                'password' => 'kasir123',
-                'role' => 'cashier'
-            ],
-            [
-                'name' => 'Agni',
-                'email' => 'agni@gmail.com',
-                'password' => 'kasir123',
+                'name' => 'Cashier',
+                'email' => 'cahier@gmail.com',
+                'password' => 'cashier',
                 'role' => 'cashier'
             ],
             [
                 'name' => 'Admin',
                 'email' => 'admin@gmail.com',
-                'password' => 'admin123',
-                'role' => 'admin'
-            ],
-            [
-                'name' => 'Dev Admin',
-                'email' => 'gatot@gmail.com',
-                'password' => 'BN123',
+                'password' => 'admin',
                 'role' => 'admin'
             ],
         ];
